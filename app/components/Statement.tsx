2b4ddeb437c49889de331948dff4f9d15e8dc1f0
Daniel Nguyen <danyen125@gmail.com>
2023-11-02 14:07:31 -0700

    change statement font size

--- app/components/Statement.tsx
+++ app/components/Statement.tsx
@@ -18,7 +18,7 @@ export default function Statement() {
 			<motion.div
 				ref={ref}
 				style={{ y: yd, opacity }}
-				className='relative top-24 flex flex-col text-[7vh] leading-relaxed'>
+				className='relative top-24 flex flex-col text-[6vh] leading-relaxed'>
 				<motion.div className='flex flex-row justify-center'>
 					<h1>A&nbsp;</h1>
 					<span className='font-sfital common-ligatures ss-01'>
