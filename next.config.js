ff86a004bcb1222c437cfb9f9ee29aa70016383b
Daniel Nguyen <danyen125@gmail.com>
2025-01-31 15:00:42 -0800

    test fix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
-/** @type {import('next').NextConfig} 
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;*/
-
-module.exports = {
-	webpack: (config) => {
-		config.optimization.splitChunks = {
-			chunks: 'all',
-			minSize: 20000,
-			maxSize: 70000, // Prevents oversized bundles
-		};
-		return config;
-	},
-	experimental: {
-		workerThreads: false, // Reduce build memory usage
-		cpus: 1, // Use a single CPU to lower memory consumption
-	},
-};
+module.exports = nextConfig;
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+	plugins: {
+		'@tailwindcss/postcss': {},
+		autoprefixer: {},
+	},
+};
