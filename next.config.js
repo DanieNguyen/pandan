02603371309a2d838ff21c3b21e727b87709638a
Daniel Nguyen <danyen125@gmail.com>
2025-01-31 14:46:03 -0800

    modify config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ const nextConfig = {};
 
 module.exports = nextConfig;*/
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({});
+module.exports = {
+	webpack: (config) => {
+		config.optimization.splitChunks = {
+			chunks: 'all',
+			minSize: 20000,
+			maxSize: 70000, // Prevents oversized bundles
+		};
+		return config;
+	},
+	experimental: {
+		workerThreads: false, // Reduce build memory usage
+		cpus: 1, // Use a single CPU to lower memory consumption
+	},
+};
